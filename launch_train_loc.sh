4b4835ed1495ff5a2cdb0fa99ac2cb32a3b8a663
Aristides Milios <aristides.milios@gmail.com>
2025-07-17 16:22:34 +0000

    launch script change

--- launch_train_loc.sh
+++ launch_train_loc.sh
@@ -1,3 +1,3 @@
-JOB_NAME=localization_training_multi
+JOB_NAME=localization_training_multi_with_ndcg
 OUTPUT_DIR=/mnt/llmd/results/exps/aristides/reason/${JOB_NAME} 
-make job JOB_NAME=${JOB_NAME} ENV=prl CONDA_EXE=/opt/conda/bin/conda SNAPSHOT=1 NPROC=8 COMMAND="cd /home/toolkit/PipelineRL-SWE; python -m pipelinerl.launch --config-dir /home/toolkit/PipelineRL-SWE/conf --config-name localization output_dir=${OUTPUT_DIR} wandb.wandb_workspace_root=/mnt/llmd/results/exps wandb.wandb_project_name=starfruit"
+make job JOB_NAME=${JOB_NAME} ENV=prl CONDA_EXE=/opt/conda/bin/conda SNAPSHOT=1 NPROC=8 COMMAND="cd /home/toolkit/PipelineRL-SWE; python -m pipelinerl.launch --config-dir /home/toolkit/PipelineRL-SWE/conf --config-name localization output_dir=${OUTPUT_DIR} wandb.wandb_workspace_root=/mnt/llmd/results/exps wandb.wandb_project_name=jackalope"
