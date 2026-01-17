d416942d88c3409dbbda46240dff28217a207c48
Aristides Milios <aristides.milios@gmail.com>
2026-01-17 18:15:51 +0000

    fixing seq parallel issue

--- pipelinerl/world.py
+++ pipelinerl/world.py
@@ -161,6 +161,14 @@ class WorldMap:
             desired_preprocessor_gpu_share = int(fixed_preprocessor)
             desired_finetune_gpu_share = int(fixed_finetune)
             expert_llm_gpus = int(fixed_expert)
+            if cfg.finetune.seq_parallel != desired_finetune_gpu_share:
+                logger.warning(
+                    "fixed_gpus.finetune=%d; setting seq_parallel=%d (was %d)",
+                    desired_finetune_gpu_share,
+                    desired_finetune_gpu_share,
+                    cfg.finetune.seq_parallel,
+                )
+                cfg.finetune.seq_parallel = desired_finetune_gpu_share
         else:
             fraction_sum = cfg.world.actor_fraction + cfg.world.preprocessor_fraction + cfg.world.finetune_fraction
             actor_fraction = cfg.world.actor_fraction / fraction_sum
